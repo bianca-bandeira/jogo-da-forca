e153ad9f3bf757da05c0622f52572f1280bbc4f8
Ludmilla <ludmillajudaico@gmail.com>
2026-04-01 21:07:52 -0300

    Estrutura inicial do codigo

--- src/JogoDaForca.java
+++ src/JogoDaForca.java
@@ -1,9 +1,14 @@
 import java.io.*;
 import java.util.*;
 
+import javax.swing.JOptionPane;
+
 public class JogoDaForca{
 
 	public JogoDaForca() {
+		ArrayList<String> palavras = new ArrayList<>();
+		ArrayList<String> dicas = new ArrayList<>();
+		
 		InputStream stream = this.getClass().getResourceAsStream("/dados/palavras.txt");
 		if (stream == null) {
 		JOptionPane.showMessageDialog(null, "Arquivo de palavras inexistente!");
@@ -13,15 +18,61 @@ public class JogoDaForca{
 		String linha;
 		while (arquivo.hasNext()) {
 			linha = arquivo.nextLine();
-			//processar linha
+			String[] divisao = linha.split(";");
+			palavras.add(divisao[0]);
+			dicas.add(divisao[1]);
 		}
 		arquivo.close();
-		System.out.println(arquivo);
 	}
 	
 	public void iniciar() {
-		new JogoDaForca();
+		//
+		//incompleto
 	}
 	
+	public String getDica() {
+		return null;
+		//incompleto
+	}
+	
+	public String getPalavra() {
+		return null;
+		//incompleto
+	}
+	
+	public ArrayList<String> getResultados() {
+		return null;
+		//incompleto
+	}
+	
+	public ArrayList<Integer> getOcorrencias(String letra) throws Exception {
+		return null;
+		//incompleto
+	}
+	
+	public boolean terminou() {
+		return false;
+		//incompleto
+	}
+	
+	public int getAcertos() {
+		return 0;
+		//incompleto
+	}
+	
+	public int getCodigoPenalidade() {
+		return 0;
+		//incompleto
+	}
+	
+	public String getNomePenalidade() {
+		return null;
+		//incompleto
+	}
+	
+	public String getResultado() {
+		return null;
+		//incompleto
+	}
 }
 
